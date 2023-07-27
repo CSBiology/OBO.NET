184c20aaa73fa4b82e78bb77f5f332c0a568d5f8
omaus <omaus@bio.uni-kl.de>
2023-07-27 19:23:52 +0200

    Add `OboEntry` type

---
+++ src/FsOboParser/OboEntry.fs
@@ -0,0 +1,7 @@
+﻿namespace FsOboParser
+
+
+/// Model of raw OboEntries, divided into Terms (as `OboTerm`s) and TypeDefs (as `OboTypeDef`s).
+type OboEntry =
+    | Term of OboTerm
+    | TypeDef of OboTypeDef
