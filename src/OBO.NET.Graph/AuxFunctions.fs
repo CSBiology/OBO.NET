b8ff0167ec7bae6d6888ba17cbcf3f357bfd3794
Oliver Maus <oliver.maus@eclipso.de>
2024-10-29 13:32:13 +0100

    Add file for auxiliary functions

---
+++ src/OBO.NET.Graph/AuxFunctions.fs
@@ -0,0 +1,15 @@
+﻿namespace OBO.NET
+
+
+[<AutoOpen>]
+module AuxFunctions =
+
+    module Seq =
+
+        let reducei (reduction : int -> 'T -> 'T -> 'T) source =
+            let mutable i = 0
+            Seq.reduce (
+                fun a b -> 
+                    i <- i + 1
+                    reduction i a b
+            ) source
