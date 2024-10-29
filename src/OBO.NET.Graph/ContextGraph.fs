8eaf6e13492e150ee89d448cf00ef262fa46e936
Oliver Maus <oliver.maus@eclipso.de>
2024-10-29 13:32:34 +0100

    Add basic unfinished context graph functionality (WIP)

---
+++ src/OBO.NET.Graph/ContextGraph.fs
@@ -0,0 +1,28 @@
+﻿namespace OBO.NET
+
+
+open Graphoscope
+open FSharpAux
+
+
+module ContextGraph =
+
+    let ontologiesToContextGraph (ontologies : OboOntology seq) =
+
+        let graphedOntos = ontologies |> Seq.map (fun onto -> onto.Ontology, Graph.ontologyToFGraph true onto)
+        let gOwithNameNodes = 
+            graphedOntos 
+            |> Seq.mapi (
+                fun i (name,ontoGr) -> 
+                    let defName = Option.defaultValue $"unnamed ontology {i}" name
+                    let nodeKeys, _ = FGraph.getNodes ontoGr |> Seq.unzip
+                    FGraph.addNode defName (OboTerm.Create defName) ontoGr |> ignore
+                    let nodeKeysAndEdges = nodeKeys |> Seq.map (fun nk -> defName, nk, "part_of")
+                    FGraph.addEdges nodeKeysAndEdges ontoGr
+            )
+
+        ontologies
+        |> Seq.reducei (
+            fun onto1 onto2 ->
+                FGraph.addNode (onto.Ontology |> Option.defaultValue $"unnamed Ontology {i}")
+        ) headGraph
